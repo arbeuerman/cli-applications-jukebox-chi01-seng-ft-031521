598f996db1ae4dc67e73ef3df0f9a8dcb679b74d
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-03-14 19:57:39 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -45,7 +45,7 @@ def play(songs)
   input = get_user_input
   output_message = "Invalid input, please try again"
   songs.each_with_index do |song, index|
-    if input == song || input==index + 1]
+    if input == song || input == index + 1
       binding.pry 
       output_message = "Playing #{song}"
     end 
